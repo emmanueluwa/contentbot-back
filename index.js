d065bbf2814d8a142918ac8dcb776fa9bd9a46e6
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-11-13 21:46:24 +0000

    updating cors config

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "*",
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
   })
 );
 
